6d19795011a81ba0b615353ccfe579978f1c2358
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-06 14:23:14 -0300

    chore: config jest

--- api/jest.config.mjs
+++ api/jest.config.mjs
@@ -1,6 +1,27 @@
 export default {
   clearMocks: true,
+  restoreMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: [
+    'text',
+    'lcov'
+  ],
+  testEnvironment: 'node',
+  coverageThreshould: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  watchPathIgnorePatterns: [
+    'node_modules'
+  ],
+  transformIgnorePatterns: ['node_modules'],
+  collectCoverageFrom: [
+    'src/**/*.js', '!src/**/index.js'
+  ]
 };
