cb672cc90f5f7e4b904313f8629d3014c5b9370d
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-07 09:36:57 -0300

    feat: FileHelper

--- api/src/fileHelper.js
+++ api/src/fileHelper.js
@@ -1,5 +1,27 @@
-export class FileHelper {
-    async getFileStatus(downloadsFolder) {
+import fs from 'fs/promises'
+import prettyBytes from 'pretty-bytes'
 
+export class FileHelper {
+    static async getFileStatus(downloadsFolder) {
+        const currentFiles = await fs.readdir(downloadsFolder)
+        const statuses = await Promise
+            .all(
+                currentFiles
+                    .map(
+                        file => fs.promises.stat(`${downloadsFolder}/${file}`)
+                    )
+            )
+        let filesStatuses = []
+        for (const fileIndex in currentFiles) {
+            const { birthtime, size } = statuses[fileIndex]
+            filesStatuses.push({
+                size: prettyBytes(size),
+                file: currentFiles[fileIndex],
+                lastModified: birthtime,
+                owner: process.env.USER
+            })
+        }
+
+        return filesStatuses
     }
 }
